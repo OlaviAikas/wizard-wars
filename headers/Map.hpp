d4e26e004b5ca0ddded22340de01f0b4617b76e3
Olavi <aikas.olavi@gmail.com>
2019-11-10 18:50:43 +0100

    wrote out templates

--- headers/Map.hpp
+++ headers/Map.hpp
@@ -1,11 +1,23 @@
 #pragma once
 #include <list>
 #include "Player.hpp"
+#include "MapObject.hpp"
 
 class Map {
     public:
         std::list<Player> players;
+        std::list<MapObject> spells;
+        std::list<MapObject> statics;
 
-        template <typename T> void draw_list(std::list<T>); //Iterates over a list and calls draw() of each element
-        template <typename T> void move_list(std::list<T>); //Iterates over a list and calls mmove() of each element
+        template <typename T> void draw_list(std::list<T> list) {
+            for (std::list<T>::iterator i = list.begin(); i != list.end(); i++) {
+                i.draw();
+            }
+        }
+
+        template <typename T> void move_list(std::list<T> list) {
+            for (std::list<T>::iterator i = list.begin(); i != list.end(); i++) {
+                i.move();
+            }
+        }
 };

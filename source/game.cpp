d1ceb0a300e02494c6bd6e4b7df57816eedcaca5
valtusovs <50198060+valtusovs@users.noreply.github.com>
2019-11-14 05:40:48 -0500

    resolution change for computer class vm

--- source/game.cpp
+++ source/game.cpp
@@ -38,8 +38,8 @@ int main(int argc, char **argv)
     al_set_new_display_option(ALLEGRO_VSYNC, 2, ALLEGRO_SUGGEST);
     al_set_new_bitmap_flags(ALLEGRO_MIN_LINEAR | ALLEGRO_MAG_LINEAR);
 
-    //al_set_new_display_flags(ALLEGRO_FULLSCREEN_WINDOW);
-    ALLEGRO_DISPLAY* disp = al_create_display(3200, 1800); //Change this resolution to change window size
+    al_set_new_display_flags(ALLEGRO_FULLSCREEN_WINDOW);
+    ALLEGRO_DISPLAY* disp = al_create_display(2560, 1299); //Change this resolution to change window size
     must_init(disp, "display");
     ALLEGRO_BITMAP* buffer = al_create_bitmap(1920, 1080); //Do not touch
 

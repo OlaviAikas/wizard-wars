7b4c745c9b3b3b5c8029cec98df69a6c64f89bf9
seorimpark <seorim.park@polytechnique.edu>
2019-12-12 00:29:26 +0100

    fixed rotation of the sprite and makefile problem

--- source/FirePellet.cpp
+++ source/FirePellet.cpp
@@ -4,7 +4,7 @@
 FireP::FireP(int start_x, int start_y, float dir_x, float dir_y) 
             : Projectile::Projectile(start_x, start_y, dir_x, dir_y, 20, 20, false, 20) {
     damage = 20;
-    sprite = al_load_bitmap("resources/fireProjectiles.bmp");
+    sprite = al_load_bitmap("resources/fireProjectile.bmp");
 }
 
 int FireP::get_damage() {
@@ -12,7 +12,7 @@ int FireP::get_damage() {
 }
 
 void FireP::draw(int camera_x, int camera_y) {
-    float at = atan2(dir_y,dir_x)+ALLEGRO_PI/3+ALLEGRO_PI/8;
+    float at = atan2(dir_y,dir_x);
     al_draw_rotated_bitmap(this->sprite,0,0, x - camera_x, y - camera_y, at,0);
 }
 

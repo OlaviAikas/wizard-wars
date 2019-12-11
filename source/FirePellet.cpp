738c984be58d08ffac055336b37417ee2f7b8e55
Wanqi <wanqi@WanqideMacBook-Pro.local>
2019-12-11 23:19:10 +0100

    I added firepellet bitmap

--- source/FirePellet.cpp
+++ source/FirePellet.cpp
@@ -4,7 +4,7 @@
 FireP::FireP(int start_x, int start_y, float dir_x, float dir_y) 
             : Projectile::Projectile(start_x, start_y, dir_x, dir_y, 20, 20, false, 20) {
     damage = 20;
-    sprite = al_load_bitmap("resources/iceProjectiles.bmp");
+    sprite = al_load_bitmap("resources/fireProjectiles.bmp");
 }
 
 int FireP::get_damage() {
@@ -15,5 +15,4 @@ void FireP::draw(int camera_x, int camera_y) {
     al_draw_bitmap(this->sprite, x - camera_x, y - camera_y, 0);
 }
 
-void FireP::on_collision(MapObject other) { 
-}
+void FireP::on_collision(MapObject other) { }

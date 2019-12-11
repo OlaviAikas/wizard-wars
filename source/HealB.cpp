c152ecb504566bb0571ccbf6f09de89441177722
Wanqi <wanqi@WanqideMacBook-Pro.local>
2019-12-12 00:01:00 +0100

    add healing beam

---
+++ source/HealB.cpp
@@ -0,0 +1,20 @@
+#include "../headers/HealB.hpp"
+#include <iostream>
+
+HealB::HealB(int start_x, int start_y, float dir_x, float dir_y) 
+            : Beam::Beam(start_x, start_y, dir_x, dir_y, 2, 50, false) {
+    damage = -10;
+    sprite = al_load_bitmap("resources/HealB.bmp");
+}
+
+int HealB::get_damage() {
+    return damage;
+}
+
+void HealB::draw(int camera_x, int camera_y) {
+    //al_draw_line(float x1, float y1, float x2, float y2,
+   //ALLEGRO_COLOR color, float thickness)
+    
+}
+
+void HealB::on_collision(MapObject other) { }

303257328253a0300ca8911861358d48fbdaa06a
uranchimegz <uranchimeg.zamindii@polytechnique.edu>
2019-12-16 13:43:14 +0100

    Encoding spells

--- source/Map.cpp
+++ source/Map.cpp
@@ -5,6 +5,7 @@
 #include <allegro5/allegro.h>
 #include <allegro5/allegro_image.h>
 #include "../headers/Interface.hpp"
+#include <allegro5/allegro_primitives.h>
 
 Map::Map(const char* name, Interface *interface) {
     this->map = al_load_bitmap(name);
@@ -43,7 +44,7 @@ void Map::check_collisions() {
             }
         }
         for (std::list<MapObject*>::iterator j = statics.begin(); j != statics.end(); j++) {
-            if (**i == **j) {
+            if (*i == *j) {
                 (*i)->on_collision(**j);
                 (*j)->on_collision(**i);
             }
@@ -73,20 +74,60 @@ std::list<Player*>::iterator Map::fetch_pit(short n) {
     return it;
 }
 
-void decode_players(std::string mes_get){
-    std::vector<std::string> mes;
-    boost::split(mes, mes_get, boost::is_any_of("."));
+
+std::string Map::encode_players(){
+    std::string to_transmit="";
+    for (std::list<Player>::iterator i = players.begin(); i != players.end(); i++){
+        std::stringstream ss;
+        ss << i->get_number(); //Write the number
+        std::string number = ss.str();
+        ss << i->get_x(); //Write the x
+        std::string x = ss.str();
+        ss << i->get_y(); //Write the y
+        std::string y = ss.str();
+        ss << i->get_destx(); //Write the destx
+        std::string destx = ss.str();
+        ss << i->get_desty(); //Write the desty
+        std::string desty = ss.str();
+        to_transmit="0"+number+"."+x+"."+y+"."+destx+"."+desty+":";
+    }
+    return to_transmit;
+}
+std::string Map::encode_controlpoint(){
+    std::string to_transmit="";
+    Controlpoint::Controlpoint i;
+    std::stringstream ss;
+    ss << i->get_number();        
+    std::string number = ss.str();
+    ss << i->get_owner();
+    std::string owner = ss.str();
+    ss << i->get_timegot();
+    std::string timegot = ss.str();
+    ss << i->get_timetoget();
+    std::string timetoget = ss.str();      
+    to_transmit="1"+owner+"."+timegot+"."+timetoget+"."+number+":";
+    }
+    return to_transmit;git 
+
 
 }
 
-void decode_controlpoint(std::string mes_get){
-    std::vector<std::string> mes;
-    boost::split(mes, mes_get, boost::is_any_of("."));
+std::string Map::encode_spells(){
+    std::string to_transmit="";
+    for (std::list<Spells>::iterator i = spells.begin(); i != spells.end(); i++){
+        std::stringstream ss;
+        //ss << i->get_number(); 
+        //std::string number = ss.str();
+        ss << i->get_x(); 
+        std::string x = ss.str();
+        ss << i->get_y(); 
+        std::string y = ss.str();
+        ss << i->get_dir_x(); 
+        std::string dir_x = ss.str();
+        ss << i->get_desty(); 
+        std::string desty = ss.str();
+        to_transmit="0"+"."+x+"."+y+"."+dir_x+"."+dir_y+":";
 
 }
-
-void decode_controlpoint(std::string mes_get){
-    std::vector<std::string> mes;
-    boost::split(mes, mes_get, boost::is_any_of("."));
-
 }
+        

9ee47f6a627ea4c59f3538d44679474556cd2930
paulbalavoine <paul.balavoine@polytechnique.edu>
2020-01-12 09:43:54 -0500

    Add encoding and deconding of zone

--- headers/Zone.hpp
+++ headers/Zone.hpp
@@ -7,6 +7,7 @@ class Zone: public Spell {
         Zone(int start_x, int start_y, int width, int height, bool noclip);
         virtual void draw(int camera_x, int camera_y);
         virtual void move();
+        std::string Zone::encode_spell();
     protected:
         int time;
         bool hit_animation;
--- source/Server.cpp
+++ source/Server.cpp
@@ -87,7 +87,6 @@ std::string Server::generateResponse(std::string message){
 
     if(message.find("thisisspell") != std::string::npos){
         (this->map)->decode_spells(message);
-        return("roger");
     }
     //return "ok Boomer";
     /*if(std::stoi(mes[0])==1){
